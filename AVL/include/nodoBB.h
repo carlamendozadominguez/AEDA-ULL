3aea0de46055ab59e6c52c81916b3cd323445f5d
Carla Mendoza Dominguez <carlamendozadominguez@gmail.com>
2018-05-15 19:08:10 +0100

    Update nodoBB.h

--- AVL/include/nodoBB.h
+++ AVL/include/nodoBB.h
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-/* 
+/*
  * File:   NodoBB.h
  * Author: carla
  *
@@ -21,72 +21,71 @@ using namespace std;
 
 template <class T>
 class nodoBB {
-    
-    public:
-        T dato_;  // Valor del contenido del nodo
-        nodoBB<T> *izq_;  // Nodo hijo izquierdo
-        nodoBB<T> *dch_;  // Nodo hijo derecho
-        int factor_balanceo;
-        
-        public:
-            /* Constructor por defecto */
-             nodoBB (void){
-                izq_ = NULL;
-                dch_ = NULL;
-            }
-            
-            /* Constructor parametrizado */
-             nodoBB (const T dato, nodoBB<T> *izq=NULL, nodoBB<T> *dch=NULL){
-                dato_ = dato;
-                izq_ = izq;
-                dch_ = dch;
-            }
-            
-            /* Destructor */
-             ~nodoBB (void){
-                if (izq_ != NULL){
-                    delete izq_;
-                    izq_ = NULL;
-                }
-                if (dch_ != NULL){
-                    delete dch_;
-                    dch_ = NULL;
-                }
-            }
-            
-           T get_dato (void){
-                return dato_;
-            }
-            
-           void set_dato (T dato){
-                dato_ = dato;
-            }
-            
-           nodoBB<T>*& get_izq (void){
-                return izq_;
-            }
-            
-           nodoBB<T>*& get_dch (void){
-                return dch_;
-            }
-            
-        
-            int get_factor(){
-                return factor_balanceo;
-            }
-            
-            void set_factor(int f){
-                factor_balanceo =f;
-            }
-          
-           void mostrar(void) const {
-                if (this == NULL){
-                    cout << "[.]";
-                }
-                else{
-                    cout << "[" << dato_ << "]";
-                }
-            }
+
+  public:
+    T dato_;  // Valor del contenido del nodo
+    nodoBB<T> *izq_;  // Nodo hijo izquierdo
+    nodoBB<T> *dch_;  // Nodo hijo derecho
+    int factor_balanceo;
+
+  public:
+    /* Constructor por defecto */
+    nodoBB (void) {
+        izq_ = NULL;
+        dch_ = NULL;
+    }
+
+    /* Constructor parametrizado */
+    nodoBB (const T dato, nodoBB<T> *izq=NULL, nodoBB<T> *dch=NULL) {
+        dato_ = dato;
+        izq_ = izq;
+        dch_ = dch;
+    }
+
+    /* Destructor */
+    ~nodoBB (void) {
+        if (izq_ != NULL) {
+            delete izq_;
+            izq_ = NULL;
+        }
+        if (dch_ != NULL) {
+            delete dch_;
+            dch_ = NULL;
+        }
+    }
+
+    T get_dato (void) {
+        return dato_;
+    }
+
+    void set_dato (T dato) {
+        dato_ = dato;
+    }
+
+    nodoBB<T>*& get_izq (void) {
+        return izq_;
+    }
+
+    nodoBB<T>*& get_dch (void) {
+        return dch_;
+    }
+
+
+    int get_factor() {
+        return factor_balanceo;
+    }
+
+    void set_factor(int f) {
+        factor_balanceo =f;
+    }
+
+    void mostrar(void) const {
+        if (this == NULL) {
+            cout << "[.]";
+        } else {
+            cout << "[" << dato_ << "]";
+        }
+    }
 };
 
 #endif /* NODOBB_H */
